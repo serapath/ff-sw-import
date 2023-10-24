bfa1678c1044adc41d6c95b14b9da43ecdd4cee5
serapath <dev@serapath.de>
2023-10-24 04:15:11 +0100

    fix url

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 if (location.port && location.hostname !== 'localhost') location.hostname = 'localhost'
 else {
   console.log('BOOT')
-  await import(`${location.origin}/io.js`)
+  const url = new URL(`${location.origin}${location.pathname}`)
+  url.pathname += 'io.js'
+  console.log(url.href)
+  await import(url.href)
   io(import.meta.url)
 }
