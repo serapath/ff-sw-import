6a21eaa53a3dfe0a9e5aba02d54999684fb18e19
serapath <dev@serapath.de>
2023-10-24 04:05:01 +0100

    fix url redirect

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (location.hostname !== 'localhost') location.hostname = 'localhost'
+if (location.port && location.hostname !== 'localhost') location.hostname = 'localhost'
 else {
   console.log('BOOT')
   await import('http://localhost:33333/io.js')
